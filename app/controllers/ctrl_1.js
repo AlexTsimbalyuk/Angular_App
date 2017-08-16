9a96a6010ec70b56a11cb9e7a5edcd1b2ec25e49
Alexander Tsimbalyuk <a.tsimbalyuk1@gmail.com>
2017-08-16 17:04:01 +0300

    Finishing the scripts for the cart. Added css media-rules.

--- app/controllers/ctrl_1.js
+++ app/controllers/ctrl_1.js
@@ -3,18 +3,31 @@ store_app.controller("ctrl_1", function($scope){
 	$scope.data = model; // каталог товаров
 
 	$scope.addToBag = function(i) { 
-		$scope.$broadcast("selection", $scope.data[i]); // 1
+	
+		$scope.$broadcast("selection", $scope.data[i]); // 1->
 		console.log( "Название товара: " + $scope.data[i].model );
 	}
 	
-	$scope.$on("number", function(event, x){ // 4
+	$scope.$on("number", function(event, x){ // ->2
+	
+		var round = angular.element(document.querySelector(".header__bag--round"));
+		
 		$scope.sum = x; 
-		if ( $scope.sum > 0 ) {
+		
+		if ( $scope.sum > 0 && $scope.sum < 10 ) {
+			
 			$scope.removeElem = true;
+			round.css("padding", "3px 7px");
+			
+		} else if ( $scope.sum > 9 ){
+			
+			$scope.removeElem = true;
+			round.css("padding", "3px 3px");
+			
 		} else {
+			
 			$scope.removeElem = false;
+			
 		}
 	});
-
 });
-
--- app/controllers/ctrl_2.js
+++ app/controllers/ctrl_2.js
@@ -3,7 +3,7 @@ store_app.controller("ctrl_2", function($scope){
 	$scope.data_buy = []; // купленные товары
 	$scope.sum_buy = 0; // количество покупок 
 	
-	$scope.$on("selection", function(event, x){ // 2
+	$scope.$on("selection", function(event, x){ // ->1
 	
 		$scope.productBuy = x; 
 		
@@ -14,6 +14,7 @@ store_app.controller("ctrl_2", function($scope){
         });
 		
 		$scope.sum_buy = $scope.data_buy.length;
+		$scope.$emit("number", $scope.sum_buy); // 2->
 		console.log("Колличество покупок " + $scope.sum_buy);
 		
 		$scope.sumPrice = 0; // общая стоимость покупок 
@@ -22,33 +23,31 @@ store_app.controller("ctrl_2", function($scope){
 			$scope.sumPrice += $scope.data_buy[i].buyPrice;
 		}
 		console.log("Общая стоимость покупок " + $scope.sumPrice);
-		
-		$scope.$emit("number", $scope.sum_buy); // 3
 	});
 	
-	$scope.delFromBag = function(index) {
+	$scope.delFromBag = function(index){
 		
 		$scope.sumPrice -= $scope.data_buy[index].buyPrice;
-
 		$scope.data_buy.splice(index, 1);
-		
 		$scope.sum_buy = $scope.data_buy.length;
+		$scope.$emit("number", $scope.sum_buy); // 2->
+	}
+	
+	$scope.getOrder = function(){
 		
-		$scope.$emit("number", $scope.sum_buy); // 3
+		if ($scope.sum_buy > 0){
+			
+			alert("Ваш заказ принят!");
+			bag.classList.toggle("bag_visible");
+			$scope.sum_buy = 0;
+			$scope.sumPrice = 0;
+			$scope.data_buy = [];
+			$scope.$emit("number", $scope.sum_buy); // 2->
+			
+		} else {
+			
+			alert("Для оформления заказа - выберите товар");
+			bag.classList.toggle("bag_visible");
+		}
 	}
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
